37517af0d37aa666b8d0b6efe9b758cd451a4df2
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2026-03-25 14:58:31 +0100

    fix fuzzing

--- fuzz/fuzz_targets/full_pipeline.rs
+++ fuzz/fuzz_targets/full_pipeline.rs
@@ -6,7 +6,7 @@ use miette::NamedSource;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(str) = std::str::from_utf8(data) {
-        let session = Session::new(NamedSource::new(str, "fuzz.at".to_string()));
+        let session = Session::new(NamedSource::new("fuzz.at", str.to_string()));
         compile_source(session);
     }
 });
